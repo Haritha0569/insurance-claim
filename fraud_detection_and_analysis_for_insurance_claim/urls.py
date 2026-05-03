a73bcc124fec9d74d3d086c9c27ed7e889032f63
Haritha0569 <harithagulla82@gmail.com>
2026-05-03 19:57:27 +0530

    Add files via upload

---
+++ fraud_detection_and_analysis_for_insurance_claim/urls.py
@@ -0,0 +1,39 @@
+"""fraud_detection_and_analysis_for_insurance_claim URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import re_path
+from Remote_User import views as remoteuser
+from fraud_detection_and_analysis_for_insurance_claim import settings
+from Service_Provider import views as serviceprovider
+from django.conf.urls.static import static
+
+urlpatterns = [
+    re_path('admin/', admin.site.urls),
+    re_path(r'^$', remoteuser.login, name="login"),
+    re_path(r'^Register1/$', remoteuser.Register1, name="Register1"),
+    re_path(r'^Predict_Insurance_Claim_Type/$', remoteuser.Predict_Insurance_Claim_Type, name="Predict_Insurance_Claim_Type"),
+    re_path(r'^ViewYourProfile/$', remoteuser.ViewYourProfile, name="ViewYourProfile"),
+    re_path(r'^serviceproviderlogin/$', serviceprovider.serviceproviderlogin, name="serviceproviderlogin"),
+    re_path(r'^View_Remote_Users/$', serviceprovider.View_Remote_Users, name="View_Remote_Users"),
+    re_path(r'^charts/(?P<chart_type>\w+)', serviceprovider.charts, name="charts"),
+    re_path(r'^charts1/(?P<chart_type>\w+)', serviceprovider.charts1, name="charts1"),
+    re_path(r'^likeschart/(?P<like_chart>\w+)', serviceprovider.likeschart, name="likeschart"),
+    re_path(r'^Find_Insurance_Claim_Prediction_Details_Ratio/$', serviceprovider.Find_Insurance_Claim_Prediction_Details_Ratio, name="Find_Insurance_Claim_Prediction_Details_Ratio"),
+    re_path(r'^View_Insurance_Claim_Prediction_Details/$', serviceprovider.View_Insurance_Claim_Prediction_Details, name="View_Insurance_Claim_Prediction_Details"),
+    re_path(r'^Train_Test_View_Results_Details/$', serviceprovider.Train_Test_View_Results_Details, name="Train_Test_View_Results_Details"),
+    re_path(r'^Download_Trained_DataSets/$', serviceprovider.Download_Trained_DataSets, name="Download_Trained_DataSets"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
